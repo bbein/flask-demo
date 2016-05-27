4223d889bc720119669c5affb2a4dbd01e1ac730
Benjamin Bein <benjamin.bein@web.de>
2016-05-26 20:31:15 -0400

    changed back to original app

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index')
 def index():
-  return HttpResponse('Hello! ')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
